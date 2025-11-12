690f195cf25fd0318a78ab772683c6a3f947b4c1
Amanecer55 <alfaro.benjamin.t@gmail.com>
2025-11-12 05:14:34 -0600

    Update principal.blade.php

--- resources/views/empleados/principal.blade.php
+++ resources/views/empleados/principal.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('titulo', 'Sistema de Nómina')</title> 
     
     {{-- Se carga Bootstrap CSS desde CDN (se elimina el uso de asset('css/') redundante) --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" xintegrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
     {{-- Aquí puedes agregar CSS personalizado con asset() si lo necesitas --}}
     {{-- <link href="{{asset('css/estilos.css')}}" rel="stylesheet" /> --}}
@@ -52,7 +52,7 @@
 
     {{-- SCRIPTS (Bootstrap JS y jQuery) --}}
     {{-- Es mejor incluir los scripts JS al final del body --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" xintegrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 </body>
