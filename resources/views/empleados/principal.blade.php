04cb55e4b105ebccbe818bcaf062119f6e1cc53e
Amanecer55 <alfaro.benjamin.t@gmail.com>
2025-11-12 05:02:33 -0600

    Update principal.blade.php

--- resources/views/empleados/principal.blade.php
+++ resources/views/empleados/principal.blade.php
@@ -1,49 +1,59 @@
-<html>
-    <head>
-        <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" />
-        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    </head>
-    <body>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- CORREGIDO: Título dinámico para la pestaña del navegador --}}
+    <title>@yield('titulo', 'Sistema de Nómina')</title> 
+    
+    {{-- Se carga Bootstrap CSS desde CDN (se elimina el uso de asset('css/') redundante) --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" xintegrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    {{-- Aquí puedes agregar CSS personalizado con asset() si lo necesitas --}}
+    {{-- <link href="{{asset('css/estilos.css')}}" rel="stylesheet" /> --}}
 
-        <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+</head>
+<body>
+    {{-- COMIENZA LA BARRA DE NAVEGACIÓN --}}
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#" _msttexthash="381446" _msthash="72">Barra de navegación</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation" _msthidden="A" _mstaria-label="320099" _msthash="73">
+            <a class="navbar-brand" href="{{ route('inicio') }}">Sistema de Nómina</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarColor02">
-            <ul class="navbar-nav me-auto">
-                <li class="nav-item">
-                <a class="nav-link active" href="#"><font _mstmutation="1" _msttexthash="59059" _msthash="74">Hogar </font><span class="visually-hidden" _msttexthash="97968" _msthash="75">(actual)</span>
-                </a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#" _msttexthash="136006" _msthash="76">Funciones</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#" _msttexthash="95589" _msthash="77">Precios</a>
-                </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false" _msttexthash="300144" _msthash="79">Catálogos</a>
-                <div class="dropdown-menu" _msthidden="4">
-                    <a class="dropdown-item" href="{{route('altaempleado')}}" _msttexthash="76466" _msthidden="1" _msthash="80">Alta</a>
-                    <a class="dropdown-item" href="#" _msttexthash="232752" _msthidden="1" _msthash="81">Nuevo</a>
-                    <a class="dropdown-item" href="{{route('reporteempleados')}}" _msttexthash="349791" _msthidden="1" _msthash="82">Reporte</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#" _msttexthash="230529" _msthidden="1" _msthash="83">Otro</a>
-                </div>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#" _msttexthash="107419" _msthash="78">Cerrar sesión</a>
-                </li>
-            </ul>
-            
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{ route('inicio') }}">Inicio</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Catálogos</a>
+                        <div class="dropdown-menu">
+                            {{-- Rutas de tu proyecto --}}
+                            <a class="dropdown-item" href="{{route('altaempleado')}}">Alta de Empleados</a>
+                            <a class="dropdown-item" href="{{route('reporteempleados')}}">Reporte de Empleados</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Otros</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cerrar sesión</a>
+                    </li>
+                </ul>
             </div>
         </div>
-        </nav>
+    </nav>
+    {{-- TERMINA LA BARRA DE NAVEGACIÓN --}}
+
+    {{-- AQUI SE INSERTA EL CONTENIDO DE LA VISTA HIJA (ALTA, EDITA, REPORTE) --}}
+    <main class="container mt-4">
         @yield('contenido')
+    </main>
+
+    {{-- SCRIPTS (Bootstrap JS y jQuery) --}}
+    {{-- Es mejor incluir los scripts JS al final del body --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" xintegrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 </body>
 </html>
